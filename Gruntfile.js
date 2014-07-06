e3420e9d9a799c28660a3652707e44de597eb0b5
Itay Adler <itayadler@gmail.com>
2014-07-07 01:53:33 +0300

    wow bash wins

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,20 +9,6 @@ module.exports = function (grunt) {
    // }
   };
 
-  grunt.registerTask('test', function(){
-    var Promise = require('bluebird')
-    var exec = Promise.promisify(require('child_process').exec);
-    var done = this.async();
-    var versions = ['0.9.8', '0.9.16', '0.10-rc8'];
-    var promises = versions.map(function(version){
-      return exec("SAILS_VERSION=" + version + " mocha test/specs");
-    });
-    Promise.all(promises).then(function(specs){
-      grunt.log.write(specs);
-      done();
-    });
-  });
-
   grunt.initConfig(config);
   grunt.registerTask('default', []);
 };
